f1f1d307ae28cb1611a5b33efe908d8eeacec1ad
RafalDembowski <dembowskirafal96@gmail.com>
2021-11-25 23:05:34 +0100

    Added exceptionMiddleware for error global handling.

---
+++ API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Application.General;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, IHostEnvironment env)
+        {
+            _env = env;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                //here is place for a logging error
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = _env.IsDevelopment()
+                ? new ErrorDetails(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                : new ErrorDetails(context.Response.StatusCode, "Server Error");
+
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            var json = JsonSerializer.Serialize(response, options);
+
+            await context.Response.WriteAsync(json);
+        }
+
+    }
+
+}
---
+++ Application/General/ErrorDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.General
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+
+        public ErrorDetails(int statusCode, string message, string details = null)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Details = details;
+        }
+
+    }
+}
